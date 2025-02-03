a9d73047317c32703a7fe4077e43a8461f363959
NicoleC09 <nico.calderon.a@gmail.com>
2025-02-03 12:47:21 -0500

    Add User tests

--- Library/src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
+++ Library/src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
@@ -1,4 +1,66 @@
 package edu.eci.cvds.tdd.library.user;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
+
+    public UserTest() {}
+
+    @Test
+    public void mustCreateUser() {
+        assertDoesNotThrow(()->{
+            User user = new User();
+        });
+    }
+
+    @Test
+    public void shouldNotGetName() {
+        User user = new User();
+        assertNull(user.getName());
+    }
+
+    @Test
+    public void shouldGetName() {
+        User user = new User();
+        user.setName("Nicole");
+        assertEquals(user.getName(), "Nicole");
+    }
+
+    @Test
+    public void shouldSetName() {
+        
+        
+    }
+
+    @Test
+    public void shouldNotSetName() {
+        assertThrows(()->{
+            User user = new User();
+            user.setName(10);
+        });
+    }
+
+    @Test
+    public void shouldGetId() {
+        
+        
+    }
+
+    @Test
+    public void shouldNotGetId() {
+        
+        
+    }
+
+    @Test
+    public void shouldSetId() {
+        
+        
+    }
+
+    @Test
+    public void shouldNotSetId() {
+        
+        
+    }
 }
