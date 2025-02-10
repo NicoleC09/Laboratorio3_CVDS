833de71aa57634995a1b0a30e852b158cf24c4e4
NicoleC09 <nico.calderon.a@gmail.com>
2025-02-09 21:13:06 -0500

    Tests of User and Book

--- Library/src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
+++ Library/src/test/java/edu/eci/cvds/tdd/library/user/UserTest.java
@@ -23,44 +23,54 @@ public class UserTest {
     public void shouldGetName() {
         User user = new User();
         user.setName("Nicole");
-        assertEquals(user.getName(), "Nicole");
+        assertEquals("Nicole", user.getName());
     }
 
     @Test
     public void shouldSetName() {
-        
+        User user = new User();
+        assertDoesNotThrow(() ->user.setName("Nicole"));
+        assertDoesNotThrow(() -> user.setName(null));
         
     }
 
     @Test
     public void shouldNotSetName() {
-        assertThrows(()->{
-            User user = new User();
-            user.setName(10);
+        User user = new User();
+        Object invalidValue = 100;
+
+        assertThrows(ClassCastException.class, () -> {
+            user.setName((String) invalidValue);
         });
     }
 
     @Test
     public void shouldGetId() {
-        
-        
+        User user = new User();
+        user.setId("cvds10002345");
+        assertEquals(user.getId(), "cvds10002345");
     }
 
     @Test
     public void shouldNotGetId() {
-        
-        
+        User user = new User();
+        assertNull(user.getId());
     }
 
     @Test
     public void shouldSetId() {
-        
-        
+        User user = new User();
+        assertDoesNotThrow(() -> user.setId("cvds10002345"));
+        assertDoesNotThrow(() -> user.setId(null));
     }
 
     @Test
     public void shouldNotSetId() {
-        
-        
+        User user = new User();
+        Object invalidValue = 88.59;
+
+        assertThrows(ClassCastException.class, () -> {
+            user.setName((String) invalidValue);
+        });
     }
 }
